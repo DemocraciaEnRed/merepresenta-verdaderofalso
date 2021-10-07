0692b1fa6cc4be32650ba5346cde617379f97138
Nato Szmedra <nato@producteca.com>
2021-10-07 00:43:21 -0300

    feat: start to pick topic's color dynamically

--- src/components/topic/index.jsx
+++ src/components/topic/index.jsx
@@ -2,8 +2,26 @@ import _ from "lodash";
 import React from "react";
 import "./index.css"
 
-const Topic = ({ topic }) => <div className="topic">
-  <p className="topic-name">{_.toUpper(topic)}</p>
-</div>;
+const topicColors = {
+  "Frases Memorables": "FB2D11",
+  "Política Nacional": "4BD6F7",
+  "Legislativas": "F7A334",
+  "Latinoamérica": "32F991",
+  "Elecciones 2021": "9F47FC"
+};
+
+const WithTopicColor = ({ render, topic }) => {
+  const color = `#${_.get(topicColors, topic)}`;
+  return render(color);
+};
+
+const Topic = ({ topic }) => {
+  return <div className="topic">
+    <WithTopicColor
+      render={color => <p style={{ "background-color": color }} className="topic-name">{_.toUpper(topic)}</p>}
+      topic={topic}
+    />
+  </div>;
+}
 
 export default Topic;
