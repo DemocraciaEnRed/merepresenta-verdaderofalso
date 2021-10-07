6f24a515019ab91ec9a328b019b8d8283785689d
Nato Szmedra <nato@producteca.com>
2021-10-07 01:19:53 -0300

    feat: better option buttons' sizes

--- src/components/question/index.jsx
+++ src/components/question/index.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import { Button, Row } from "react-bootstrap";
 import "./index.css";
 
+const MobileSizeContainer = ({ children }) => <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6">
+  {children}
+</div>;
+
 const Option = ({ onOptionSelected, option }) =>
   <Button className="btn btn-dark option-button col-md-12" onClick={() => onOptionSelected(option)}>
     <span className="option-text">{_.toUpper(option)}</span>
   </Button>
 
 const Question = ({ question, options, onOptionSelected }) => (
-  <div>
+  <div className="col-10">
     <p>{question.text}</p>
-    <div className="col-md-8">
+    <MobileSizeContainer>
       {options.map((option, number) =>
         <Row key={number} className="option">
           <Option
@@ -21,7 +25,7 @@ const Question = ({ question, options, onOptionSelected }) => (
           />
         </Row>
       )}
-    </div>
+    </MobileSizeContainer>
   </div>
 );
 
