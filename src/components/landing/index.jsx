54b76c488a7fa198f89d52bd6066bb3348726d65
Nato Szmedra <nato@producteca.com>
2021-10-17 17:47:02 -0300

    feat: landing styling

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import WithBackground from "../withBackground";
+import Logo from "../logo";
 import Background from '../../images/initialLoadingBackground.svg';
+import "./index.css";
 
 const Landing = ({ follow }) => <WithBackground background={Background}>
-  <p>Merre</p>
+  <div className="landing-logo">
+    <Logo color="white"/>
+    <p className="game-name">TRIVIA</p>
+  </div>
   <button onClick={follow}>Next</button>
 </WithBackground>
 
