32109d3a2803976874ae26f4ad3bbdcb3e8c76d2
Nato Szmedra <nato@producteca.com>
2021-10-17 21:43:29 -0300

    fix: landing animations

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -4,13 +4,13 @@ import Logo from "../logo";
 import Background from '../../images/initialLoadingBackground.svg';
 import "./index.css";
 
-const Landing = ({ follow }) => <WithBackground background={Background}>
+const Landing = ({ follow }) => <div className="animate__animated animate__fadeOut animate__delay-3s" onAnimationEnd={follow}>
+  <WithBackground background={Background}>
   <div className="centered animate__animated animate__fadeIn animate__delay-1s">
-    <div className="animate__animated animate__fadeOut animate__delay-2s" onAnimationEnd={follow}>
       <Logo color="white"/>
       <p className="game-name">TRIVIA</p>
     </div>
-  </div>
 </WithBackground>
+</div>
 
 export default Landing;
