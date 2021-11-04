9af8824664eb02ac612311fd3423aaf8c6672988
Nato Szmedra <nato@producteca.com>
2021-11-03 23:56:04 -0300

    fix: initial views styling

--- src/components/topics/index.jsx
+++ src/components/topics/index.jsx
@@ -11,9 +11,9 @@ const Topics = ({ follow }) => <WithBackground background={Background}>
   <div className="animate__animated animate__fadeIn">
     <Logo />
     <div className="topics col-10">
-      <p className="topics-description">Te vamos a hacer 10 preguntas de estas 5 categorías:</p>
+      <p className="topics-description">Te vamos a hacer 10 preguntas. Deberás contestar si son verdaderas o falsas</p>
       <Button onClick={follow} className="col-md-3 col-sm-4 col-10 play" variant="outline-light">JUGAR</Button>
-      <p className="game-owner">Desarrollado por Directorio Legislativo y Democracia en Red</p>
+      <p className="game-owner">Desarrollo: Poder Ciudadano + Democracia en Red</p>
     </div>
   </div>
 </WithBackground>
