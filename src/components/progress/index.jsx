63e0517c087ef27e747cf7333c0a3c5103063734
Nato Szmedra <nato@producteca.com>
2021-11-03 22:54:50 -0300

    Revert "feat: remove topic abstraction from Progress"
    This reverts commit 5ded4de65a8361439ad5dac4c84604764bb525e0.

--- src/components/progress/index.jsx
+++ src/components/progress/index.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import WithTopicColor from "../withTopicColor";
 import "./index.css";
 
-const CurrentNumber = ({ color = "black", current }) => <span style={{ "color": color }}>{current}</span>;
+const CurrentNumber = ({ color, current }) => <span style={{ "color": color }}>{current}</span>;
 const OfTotal = ({ total }) => <span> de {total}</span>;
 
-const Progress = ({ current, total, final, color }) => {
-  const isFinalOne = final;
-  return <div className={"score" + " " + (isFinalOne? "final" : "partial")}>
+const Progress = ({ current, total, topic, color }) => {
+  const isFinalOne = !topic;
+  return <div className={isFinalOne? "final" : ""}>
     {
-      <CurrentNumber color={color} current={current} />
+      !isFinalOne?
+        <WithTopicColor
+          render={color => <CurrentNumber color={color} current={current}/>}
+          topic={topic}
+        />
+      : <CurrentNumber color={color} current={current} />
     }
     <OfTotal total={total} />
   </div>;
